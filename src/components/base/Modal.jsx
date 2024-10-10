e77e7827ffb9031253e411656e438c348b018a3f
findecano <jacruzjacruz@gmail.com>
2024-10-10 10:12:59 -0500

    Avance en aspecto de Modal de opciones, efecto blur de fondo para inhabilitar más acciones, colores.

--- src/components/base/Modal.jsx
+++ src/components/base/Modal.jsx
@@ -7,13 +7,17 @@ export default function Modal({onClose,children}){
         onClose();
     }
     return (
-        <div className="modal">
-            <Button
-                btnClassNames="btn-icon-1"
-                iconClassNames="fa fa-heart"
-                onPress={handleClose}
-            ></Button>
-            {children}
+        <div className='modal'>
+            <div className="modal-container modal-container-in">
+                <Button
+                    btnClassNames="btn-icon-1"
+                    iconClassNames="fa fa-times"
+                    onPress={handleClose}
+                ></Button>
+                <div className='modal-content'>
+                    {children}
+                </div>
+            </div>
         </div>
     )
 }
